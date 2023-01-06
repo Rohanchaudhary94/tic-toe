ea0587dc3d68c6dd5917ed78b95aeac009c3f266
rohan raj <write2rohan94@gmail.com>
2023-01-06 15:43:26 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React,{ useContext, useState } from 'react';
+import {AppContext} from '../App';
+import '../style/header.css';
+import Playing from './Playing';
+import End from './End';
+import Winner from './Winner';
+
+
+const Header = () => {
+    const { currentChar, winner, gameOver } = useContext(AppContext);
+  return (
+    <div className="header">
+    { !winner && !gameOver && <Playing /> }
+    { gameOver && !winner && <End /> }
+    { winner && <Winner /> }
+</div>
+  )
+}
+
+export default Header
